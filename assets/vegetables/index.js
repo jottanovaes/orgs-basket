393d19720e59296d644346ddb02c570483d96d02
jottanovaes <jottanovaes@gmail.com>
2022-03-27 14:18:56 -0300

    refactor(mock): import assets and add to mock

---
+++ assets/vegetables/index.js
@@ -0,0 +1,13 @@
+import broccoli from './broccoli.png'
+import cucumber from './cucumber.png'
+import potato from './potato.png'
+import pumpkin from './pumpkin.png'
+import tomato from './tomato.png'
+
+export default Vegetables = {
+  broccoli,
+  cucumber,
+  potato,
+  pumpkin,
+  tomato,
+}
--- src/mocks/basket.js
+++ src/mocks/basket.js
@@ -1,5 +1,7 @@
 import logo from '../../assets/logo.png';
 
+import {broccoli, cucumber, potato, pumpkin, tomato} from '../../assets/vegetables'
+
 const basket = {
   top: {
     title: 'Basket details'
@@ -10,6 +12,32 @@ const basket = {
     farmName: 'Jenny Jack Farm',
     description: 'A hand carefully picked basket products from farm directly to your kitchen',
     price: 40.00,
+    button: 'Buy'
+  },
+  items: {
+    title: 'Basket items',
+    list: [
+      {
+        name: 'Broccoli',
+        thumb: broccoli,
+      },
+      {
+        name: 'Cucumber',
+        thumb: cucumber,
+      },
+      {
+        name: 'Potatp',
+        thumb: potato,
+      },
+      {
+        name: 'Pumpkin',
+        thumb: pumpkin,
+      },
+      {
+        name: 'Tomato',
+        thumb: tomato,
+      },
+    ]
   }
 }
 
