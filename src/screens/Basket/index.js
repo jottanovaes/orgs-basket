51edaef1d43f919d0443bfe1cf90061227b2c888
jottanovaes <jottanovaes@gmail.com>
2022-03-27 15:59:02 -0300

    feat(Basket): turn Basket into a scrollable page changes were made due to items list

--- src/screens/Basket/index.js
+++ src/screens/Basket/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 import { BasketDetails } from "./styles";
+import { ScrollView } from "react-native";
 
 import Top from "./components/Top";
 import Details from "./components/Details";
+import Items from './components/Items';
 
-export default function Basket({ top, details }) {
+export default function Basket({ top, details, items }) {
   return (
-    <>
+    <ScrollView>
       <Top { ...top }/>
 
       <BasketDetails>
         <Details { ...details }/>
+        <Items {...items}/>
       </BasketDetails>
-    </>
+    </ScrollView>
   )
 }
